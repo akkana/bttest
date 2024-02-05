03a9f565b2cc4d37c49cc37ddfbabd541c5f3fc1
Akkana Peck <akkana@shallowsky.com>
2024-02-04 18:42:31 -0700

    more stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from app import routes, db
+from config import Config
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-# from flask import render_template, flash, redirect, url_for, request
-# from flask import session
-# from flask import request
-# from flask_login import login_user, logout_user, current_user, login_required
-# from werkzeug.urls import url_parse
+from flask import render_template, flash, redirect, url_for, request
+from flask import session
+from flask import request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
 
-from app import app
+from app import app, db
 
 # from app import chattycaptcha
 # from app.forms import LoginForm, RegistrationForm, AddBillsForm, \
