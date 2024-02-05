24745138491ab61106f6faa3a90a913a5767166f
Akkana Peck <akkana@shallowsky.com>
2024-02-04 18:20:22 -0700

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+# from flask import render_template, flash, redirect, url_for, request
+# from flask import session
+# from flask import request
+# from flask_login import login_user, logout_user, current_user, login_required
+# from werkzeug.urls import url_parse
+
+from app import app
+
+# from app import chattycaptcha
+# from app.forms import LoginForm, RegistrationForm, AddBillsForm, \
+#     NewTagsForm, UserSettingsForm, PasswordResetForm
+# from app.models import User, Bill, Legislator, Committee, LegSession
+# from app.bills import nmlegisbill, billutils, billrequests
+# from app.emails import send_email
+# from .routeutils import BILLNO_PAT, html_bill_table, make_new_bill, \
+#     g_all_tags, get_all_tags, group_bills_by_tag, set_session_by_request_values
+# from config import ADMINS
+
