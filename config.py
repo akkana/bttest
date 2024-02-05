7d20b9cef597ec3b8de362e70334488d0383b3d4
Akkana Peck <akkana@shallowsky.com>
2024-02-04 18:50:51 -0700

    Take out 'or' clause from db definition in config

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'NOT-REALLY-SECRET'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
-        or "sqlite:///test.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
